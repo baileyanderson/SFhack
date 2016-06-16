da54867629c1df0d6c87e0dbf4aa2c1c0dd4b1a6
Andrew Oliveira <tf2zone@gmail.com>
2016-06-16 00:18:52 -0400

    added the server and client
    added the python server and client programs to send and recive data

---
+++ python/client.py
@@ -0,0 +1,21 @@
+#! /usr/bin/python3
+
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+host = "10.3.5.59"
+port = 8080
+s.connect((host, port))
+
+
+def ts(str):
+    s.send(str.encode())
+    data = ''
+    data = s.recv(1024).decode()
+    print(data)
+
+while 2:
+    r = input()
+    ts(r)
+
+s.close()
---
+++ python/server.py
@@ -0,0 +1,44 @@
+import socket
+from threading import *
+
+serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+host = "10.3.5.59"
+port = 8080
+print(host)
+print(port)
+serversocket.bind((host, port))
+
+
+keyWords = ["claim", "auto", "car", "vehicle", "rental", "quote", "home",
+            "house", "fire", "life", "status"]
+
+
+class client(Thread):
+    def __init__(self, socket, address):
+        Thread.__init__(self)
+        self.sock = socket
+        self.addr = address
+        self.start()
+
+    def run(self):
+        while 1:
+            out = []
+            msg = self.sock.recv(1024).decode()
+            # print(msg)
+            print('Client sent:', msg)
+            for thing in keyWords:
+                if thing in msg.replace("auto", "car").replace("vehicle", "car").replace("home", "house"):
+                    out.append(thing)
+
+            print(out)
+
+            if str(msg) == str("stop"):
+                serversocket.close()
+                break
+            self.sock.send(b'\nServer Recived Msg\n')
+
+serversocket.listen(5)
+print('server started and listening')
+while 1:
+    clientsocket, address = serversocket.accept()
+    client(clientsocket, address)
